0903d435872bad1be177c416aac4eb30d0c165bf
kiran-jaswant <kiranjaswant08@gmail.com>
2024-08-22 23:48:26 +0530

    basic routing setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,36 @@
 import { useState } from 'react'
 import './App.css'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+
+import Home from './views/Home/Home'
+import About from './views/About/About'
+import Review from './views/Review/Review'
 
 function App() {
+
+  const router =createBrowserRouter([
+    {
+      path:'/',
+      element:<Home></Home>
+    },
+    {
+      path:"/about",
+      element:<About></About>
+    },
+    {
+      path:"/review",
+      element:<Review></Review>
+    }
+  ])
   
 
   return (
-    <>
-      <h1 className=''>Tailwind css</h1>
-      <h2>welcome to the hackathon</h2>
-    </>
+     <>
+     <RouterProvider router={router}></RouterProvider>
+     </>
+    
+   
   )
 }
 
-export default App
+export default App;
